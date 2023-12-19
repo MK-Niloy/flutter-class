00c66016f1c0e07eedd275111192e7d62b43c051
Niloy <mkniloy1568@gmail.com>
2023-12-19 14:32:59 +0600

    appbar_text

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,35 @@ class HomeScreen extends StatelessWidget{
   Widget build(BuildContext context) {
    return Scaffold(
      backgroundColor: Colors.amberAccent ,
+    appBar: AppBar(
+      leading: Icon(Icons.home,size: 32,color: Colors.blue,),
+      title: Text('Home' ,style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        color: Colors.blue,
+      ),),
+      backgroundColor: Colors.black,
+    ),
     body: Center(
      // child: Image.network(
        // 'https://mdbootstrap.com/docs/standard/plugins/dummy/dummy.jpg'),
-      child: Image.asset('images/photo.jpeg',
-        height: 400,
-        width: 400,
-        fit: BoxFit.fitWidth,
-          //repeat: ImageRepeat.repeat,
-
+      // child: Image.asset('images/photo.jpeg',
+      //   height: 400,
+      //   width: 400,
+      //   fit: BoxFit.fitWidth,
+      //     //repeat: ImageRepeat.repeat,
+      //
+      // ),
+      child: Text(
+        'Hello Niloy, Welcome back',
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.blue,
+          fontFamily:'Raleway',
+          decoration: TextDecoration.underline,
+          fontStyle: FontStyle.italic,
+          overflow: TextOverflow.visible,
+        ),
       ),
     ),
    );
