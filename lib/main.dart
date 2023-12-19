ecfdf856f3f5d4522300a1db6fbedd266c2ed2cd
Niloy <mkniloy1568@gmail.com>
2023-12-19 15:08:06 +0600

    column

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,48 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
+
 }
+
 class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      //title : 'To Do'
-    home: HomeScreen(),
+      home:HomeScreen(),
     );
-    // TODO: implement build
-    //throw UnimplementedError();
+    //TODO: implement build
   }
-  
 }
 
 class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     backgroundColor: Colors.amberAccent ,
-    appBar: AppBar(
-      leading: Icon(Icons.home,size: 32,color: Colors.blue,),
-      title: Text('Home' ,style: TextStyle(
-        fontSize: 32,
-        fontWeight: FontWeight.w700,
-        color: Colors.blue,
-      ),),
-      backgroundColor: Colors.black,
-    ),
-    body: Center(
-     // child: Image.network(
-       // 'https://mdbootstrap.com/docs/standard/plugins/dummy/dummy.jpg'),
-      // child: Image.asset('images/photo.jpeg',
-      //   height: 400,
-      //   width: 400,
-      //   fit: BoxFit.fitWidth,
-      //     //repeat: ImageRepeat.repeat,
-      //
-      // ),
-      child: Text(
-        'Hello Niloy, Welcome back',
-        style: TextStyle(
-          fontSize: 30,
-          color: Colors.blue,
-          fontFamily:'Raleway',
-          decoration: TextDecoration.underline,
-          fontStyle: FontStyle.italic,
-          overflow: TextOverflow.visible,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.flutter_dash, size: 32, color: Colors.amber,),
+        title: Text('Flutter' ,style: TextStyle(
+            fontSize: 32,
+          color: Colors.amber,
+
+        ),),
+        backgroundColor: Colors.black,
       ),
-    ),
-   );
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //mainAxisSize: MainAxisSize.min,
+       // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('data' ,style: TextStyle(
+            color: Colors.amber,
+            fontSize: 32,
+          ),),
+          Text('data'),
+          Text('data'),
+          Text('data'),
+        ],
+      ),
+    );
   }
 
 }
