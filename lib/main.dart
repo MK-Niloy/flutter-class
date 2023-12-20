30c13e858e6bc201d4e9d34047f208f1a9c1cf07
Niloy <mkniloy1568@gmail.com>
2023-12-21 01:56:03 +0600

    All type button

--- lib/main.dart
+++ lib/main.dart
@@ -6,57 +6,75 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home:HomeScrene(),
+        home:HomeScreen(),
     );
+
     // TODO: implement build
     throw UnimplementedError();
   }
 }
-class HomeScrene extends StatelessWidget{
+class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.green,
       appBar: AppBar(
-        leading: Icon(
-          Icons.flutter_dash_outlined, color: Colors.amber, size: 32,),
-        title: Text('Flutter', style: TextStyle(
-          fontSize: 32,
+        backgroundColor: Colors.blue,
+        title: Text('Home',style: TextStyle(
           color: Colors.amber,
+          fontSize: 32,
         ),),
-        backgroundColor: Colors.black,
       ),
-
-      body: Column(
-
-        //mainAxisSize: MainAxisSize.max,
-        //mainAxisAlignment: MainAxisAlignment.center,
-       // crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('Data'),
-          Text('Data'),
-          Text('Data'),
-          Text('Data'),
-          ElevatedButton(onPressed: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (_) {
-                  return Column(
-                    children: [
-                      Text('Message'),
-                      Text('Hello world'),
-                    ],
-                  );
-                });
-          },
-              child: Text('To Do'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){
+              print('See Now');
+            }, child: Text('Home'),
+              onLongPress: (){
+              print('It is long press');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor:Colors.amber,
+                foregroundColor: Colors.white,
+                // minimumSize: Size(300, 40),
+                // maximumSize: Size(300, 80),
+                //padding: EdgeInsets.all(15),//impotant key
+               // padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+               // padding: EdgeInsets.only(left: 12, top: 12,right: 20,bottom: 13),
+                elevation: 15,//shadow
+                shadowColor: Colors.blue,
+                textStyle: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                ),
+                shape: RoundedRectangleBorder(
+                 // borderRadius: BorderRadius.zero,
+                  borderRadius: BorderRadius.circular(10),
+                //  borderRadius: BorderRadius.all(),
+                ),
+              ),
+            ),
+            TextButton(onPressed: (){
+              print('Text Button');
+            },
+                child: Text('Text Button'),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                //elevated button er moto sob style kora jabe
+              ),
+            ),
+            IconButton(onPressed: (){}, icon: Icon(Icons.mobile_friendly),),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
       ),
     );
   }
-
-
-
 
 }
