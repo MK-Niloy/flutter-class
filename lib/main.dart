f013d27172ab08516a62cdd27c8210b234a51b80
Niloy <mkniloy1568@gmail.com>
2023-12-21 00:35:08 +0600

    bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -15,52 +15,48 @@ class MyApp extends StatelessWidget{
 class HomeScrene extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     backgroundColor: Colors.blue,
-     appBar: AppBar(
-       leading: Icon(Icons.flutter_dash_outlined ,color: Colors.amber, size: 32,),
-       title:Text('Flutter', style: TextStyle(
-         fontSize: 32,
-         color: Colors.amber,
-       ),),
-       backgroundColor: Colors.black,
-     ),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        leading: Icon(
+          Icons.flutter_dash_outlined, color: Colors.amber, size: 32,),
+        title: Text('Flutter', style: TextStyle(
+          fontSize: 32,
+          color: Colors.amber,
+        ),),
+        backgroundColor: Colors.black,
+      ),
 
-     body: Column(
+      body: Column(
 
-       mainAxisSize: MainAxisSize.max,
-       mainAxisAlignment: MainAxisAlignment.center,
-       crossAxisAlignment: CrossAxisAlignment.center,
-       children: [
-         Text('Data'),
-         Text('Data'),
-         Text('Data'),
-         Text('Data'),
-         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Text('Data'),
-             Text('Data'),
-             Text('Data'),
-             Text('Data'),
-           ],
-
-         ),
-         ElevatedButton(onPressed: (){
-            showDialog(context: context, builder: (context){
-              return AlertDialog(
-                title: Text('Message'),
-                content: Text('Welcome back'),
-                actions: [
-                  ElevatedButton(onPressed: (){}, child: Text('Cancel'),),
-                ],
-              );
-            });
-         },
-             child: Text('To Do'))
+        //mainAxisSize: MainAxisSize.max,
+        //mainAxisAlignment: MainAxisAlignment.center,
+       // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Data'),
+          Text('Data'),
+          Text('Data'),
+          Text('Data'),
+          ElevatedButton(onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (_) {
+                  return Column(
+                    children: [
+                      Text('Message'),
+                      Text('Hello world'),
+                    ],
+                  );
+                });
+          },
+              child: Text('To Do'),
+          ),
         ],
-     ),
-   );
+      ),
+    );
   }
+
+
+
 
 }
